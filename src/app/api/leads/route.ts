e8f09189d075673c12d3299a999243874ae3c6de
ivanfgavilan <ivanfgavilan@gmail.com>
2026-04-20 16:59:28 -0300

    Update route.ts

--- src/app/api/leads/route.ts
+++ src/app/api/leads/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
+import { getServerSession } from "next-auth";
 
 export async function POST(request: Request) {
   try {
